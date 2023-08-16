70ec755f0c632a465c01c4291a6efd21f7714add
Jepkor1r <jlagat007@gmail.com>
2023-08-16 18:20:55 +0300

    updated

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- List all priviledges
-SHOW GRANTS FOR IF EXISTS 'user_0d_1'@'localhost';
+ IF EXISTS
+    SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- SHOW GRANTS IF EXISTS FOR 'user_0d_2'@'localhost';
