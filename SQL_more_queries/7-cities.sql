fcd16b7fe81ed2b6103f6ebcabd2dc95a60f5646
Jepkor1r <jlagat007@gmail.com>
2023-08-17 18:11:32 +0300

    updated

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,8 @@
-CREATE SCHEMA hbtn_0d_usa
-CREATE TABLE cities(
-id INT UNIQUE AUTO NOT NULL PRIMARY KEY,
+-- Creating a database named hbtn_0d_usa
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa
+USE hbtn_0d_usa
+-- Creating a table named cities
+CREATE TABLE IF NOT EXISTS cities(
+id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id)
 name VARCHAR(256));
