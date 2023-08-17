5eb10225acf4e5ef9f235e36b2e23bdcb205446c
Jepkor1r <jlagat007@gmail.com>
2023-08-17 18:01:29 +0300

    updated

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Creating a database named hbtn_0d_usa
-CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
+CREATE SCHEMA IF EXISTS hbtn_0d_usa;
 -- Creating a table named states
 CREATE TABLE IF NOT EXISTS states(
 id INT UNIQUE AUTO NOT NULL PRIMARY KEY,
