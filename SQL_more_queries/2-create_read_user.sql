e2f6c2fb99b694c5e30c336746930c00110fba2c
Jepkor1r <jlagat007@gmail.com>
2023-08-17 17:53:57 +0300

    updated

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- creating database
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
 -- granting the user all priviledges to the server
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
