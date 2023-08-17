7a5e6d5b603c5e4843a2b14860d05a9cda2bcd3d
Jepkor1r <jlagat007@gmail.com>
2023-08-17 16:53:25 +0300

    updated

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 --creating user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
-
 --granting the user all priviledges to the server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
